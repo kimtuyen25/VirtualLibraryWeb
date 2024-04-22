bb4bc9210fab2f0d4b6343171a163d63839cf573
kimtuyen2511 <nguyentuyentctv2021@gmail.com>
2024-04-22 20:09:47 +0700

    index

---
+++ js/home.js
@@ -0,0 +1,37 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/home", {
+      templateUrl: "./html/user/main.html"
+    })
+    .when("/category", {
+      templateUrl: "./html/user/listBooks.html"
+
+    })
+    .when("/history", {
+      templateUrl: "./html/user/historyRead.html"
+
+    })
+    .when("/favorite", {
+      templateUrl: "./html/user/favoriteBooks.html",
+    })
+    .when("/detailsBook", {
+      templateUrl: "./html/user/bookDetails.html",
+    })
+    .when("/login", {
+      templateUrl: "./html/account/login.html",
+    })
+    .when("/register", {
+      templateUrl: "./html/account/register.html",
+    })
+    .when("/updateProfile", {
+      templateUrl: "./html/account/updateProfile.html",
+    })
+    .when("/changePass", {
+      templateUrl: "./html/account/changePass.html",
+    })
+    .otherwise({
+      redirectTo: "/home"
+    })
+
+  });
