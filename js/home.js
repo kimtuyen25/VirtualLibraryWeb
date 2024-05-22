8e3b23a468a4016fd34e4507d0253da4b1f23b34
phuckemz03 <146057737+phuckemz03@users.noreply.github.com>
2024-05-22 22:40:40 +0700

    HomeBanHang-donHang-ChiTietDonHang-DanhGia-NganHang
    HomeBanHang-donHang-ChiTietDonHang-DanhGia-NganHang

--- js/home.js
+++ js/home.js
@@ -27,9 +27,52 @@ app.config(function ($routeProvider) {
       templateUrl: "./html/user/postBooks.html",
     })
     .when("/buyBook", {
-      templateUrl: "./html/user/buyBooksHome.html",
+      templateUrl: "./html/user/demo.html",
     })
     .otherwise({
       redirectTo: "/home"
     })
+});
+app.controller('SliderController', function($scope) {
+  // Controller logic if needed
+});
+
+app.directive('draggableSlider', function() {
+  return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+          let isDown = false;
+          let startX;
+          let scrollLeft;
+
+          element.on('mousedown', function(e) {
+              isDown = true;
+              startX = e.pageX - element[0].offsetLeft;
+              scrollLeft = element[0].scrollLeft;
+              element.addClass('active');
+              console.log("mousedown");
+          });
+
+          element.on('mouseleave', function() {
+              isDown = false;
+              element.removeClass('active');
+              console.log("mouseleave");
+          });
+
+          element.on('mouseup', function() {
+              isDown = false;
+              element.removeClass('active');
+              console.log("mouseup");
+          });
+
+          element.on('mousemove', function(e) {
+              if (!isDown) return;
+              e.preventDefault();
+              const x = e.pageX - element[0].offsetLeft;
+              const walk = (x - startX) * 2; // The multiplication factor controls the speed of the scroll
+              element[0].scrollLeft = scrollLeft - walk;
+              console.log("mousemove");
+          });
+      }
+  };
 });
